0abdea66a043c2b91cf3b1bfc949386b5494c0fe
Anindita Chakraborty <aninditac1912@gmail.com>
2026-01-09 23:39:31 +0530

    minor mistakes fix

--- frontend/src/components/signup/SignupPage.jsx
+++ frontend/src/components/signup/SignupPage.jsx
@@ -1,5 +1,6 @@
 import Signup from "./SignUp";
 import hero from "../../assets/Hero.jpg"
+import logo from "../../assets/logo.svg"
 export default function SignupPage(){
     return(
         <div className="container">
@@ -7,7 +8,7 @@ export default function SignupPage(){
             <div className="row">
                 <div className="col-sm-12 col-lg-6 ">
                     <div>
-                        <img src="public/images/logo.svg" className="object-fit-contain " style={{width:"150px"}}></img>
+                        <img src={logo} className="object-fit-contain " style={{width:"150px"}}></img>
                     </div>
                     <img src={hero} className="object-fit-contain mt-5 pt-3" style={{width:"100%"}}></img>
 
